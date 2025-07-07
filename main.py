354efe49ef51d76224dcb74158a85f0692ed56b4
ProVeg <michaellawrencemorgan@gmail.com>
2025-07-07 00:29:43 -0700

    Update main.py
    Changed the end of the code for port access

--- main.py
+++ main.py
@@ -55,4 +55,6 @@ def serve_privacy():
     return send_from_directory(".", "privacy.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
